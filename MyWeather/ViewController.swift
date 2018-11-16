8d42cbd8819d6b6ff89ceca14c49c8d3d3243d21
Peter Centellini <peter.centellini@redesajn.com>
2018-11-16 18:17:26 +0100

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Peter Centellini on 2018-11-16.
+//  Copyright © 2018 Redesajn Interactive Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
